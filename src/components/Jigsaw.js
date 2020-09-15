0c7cafd8f0ef63aebf53601a25881aafd2ef00e9
justha <nganha42@gmail.com>
2020-09-15 08:43:53 -0500

    initial commit

---
+++ src/components/Jigsaw.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Jigsaw.css"
+
+export const Jigsaw = () => (
+    <>
+    <h1>puzzl</h1>
+    <small>an app for avid jigsaw puzzlers</small>
+    {/* <br></br>
+    <small>log. plan. trade.</small> */}
+    </>
+)
